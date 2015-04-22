d155aeb5a158c0faed8f9069f37dc096fcc7c6d3
Artur Fenko <artur.fenko@itechart-group.com>
2015-04-23 00:20:52 +0300

    uncurring simplified

--- src/chapter1/Ex2d4.scala
+++ src/chapter1/Ex2d4.scala
@@ -2,8 +2,8 @@ package chapter1
 
 object Ex2d4 extends App {
   //reverses the transformation of curry
-  def uncurry[A,B,C](f: A => (B => C)): (A, B) => C = {
-    (a : A, b : B) => f(a)(b)
+  def uncurry[A,B,C](f: A => B => C): (A, B) => C = {
+    (a, b) => f(a)(b)
   }
 
   val func = uncurry[String, String, String](a => b => a + b)
