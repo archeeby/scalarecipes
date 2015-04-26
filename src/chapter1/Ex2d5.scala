21263cbd1de6e2ffea3f86510bdae43a4c77c1d0
Artur Fenko <artur.fenko@itechart-group.com>
2015-04-26 16:09:49 +0300

    compose example

---
+++ src/chapter1/Ex2d5.scala
@@ -0,0 +1,17 @@
+package chapter1
+
+object Ex2d5 extends App {
+  def compose[A,B,C](f: B => C, g: A => B): A => C = {
+    a => f(g(a))
+  }
+  //-----------------------
+  def func1(x: Int) = 2 * x   //f
+  def func2(y: Int) = 1 + y   //g
+
+  val a = 5
+  val b = func2(a)
+  val c = func1(b)
+
+  println(c)
+  println(func1(func2(a)))
+}
