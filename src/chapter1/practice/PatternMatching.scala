c9e2b93b362734533d4a6ef2c3eba2e48d58fa64
Artur Fenko <artur.fenko@itechart-group.com>
2015-05-05 13:34:57 +0300

    pattern matching

---
+++ src/chapter1/practice/PatternMatching.scala
@@ -0,0 +1,34 @@
+package chapter1.practice
+
+sealed trait List[+A]
+
+case object Nil extends List[Nothing]
+
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List {
+  def sum(ints: List[Int]): Int = ints match {
+    case Nil => 0
+    case Cons(x,xs) => x + sum(xs)
+  }
+  def product(ds: List[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x,xs) => x * product(xs)
+  }
+  def apply[A](as: A*): List[A] =
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+}
+
+object PatternMatching extends App {
+  val list = List(1, 2, 3, 4)
+  val doubleList = List(0.4, 0.1, 1.0, 0.9)
+  val doubleListWithZero = List(0.4, 0.1, 0.0, 1.0, 0.9)
+
+  println(List.sum(list))
+
+  println(List.product(Nil))
+  println(List.product(doubleListWithZero))
+  println(List.product(doubleList))
+}
